989b1225aac06c4e006039a6db499434d7e0c38c
Vichar Nuchsiri [620335] <vichar.nuchsiri@ktb.co.th>
2019-10-11 15:13:20 +0700

    refactoring graphic

--- Grid.go
+++ Grid.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Grid struct {
 	width  int
 	height int
@@ -7,8 +9,13 @@ type Grid struct {
 }
 
 func (g Grid) LoadOne(x int, y int, value bool) {
-	row := make([]bool, g.width)
-	row[x] = value
+	var row []bool
+	if g.pixels[y] == nil {
+		row = make([]bool, g.width)
+	} else {
+		row = g.pixels[y]
+	}
+	row[x] = row[x] || value
 	g.pixels[y] = row
 }
 
@@ -19,10 +26,9 @@ type Location struct {
 }
 
 func GridInit(w int, h int) *Grid {
-
-	p := make([][]bool, w)
+	p := make([][]bool, h)
 	for i := range p {
-		p[i] = make([]bool, h)
+		p[i] = make([]bool, w)
 	}
 	g := Grid{width: w, height: h, pixels: p}
 	return &g
@@ -31,5 +37,14 @@ func GridInit(w int, h int) *Grid {
 func (g Grid) Load(locations []Location) {
 	for _, location := range locations {
 		g.LoadOne(location.x, location.y, location.pixel)
+	}
+}
+
+func (g Grid) Evaluate() {
+	for i := 0; i < len(g.pixels); i++ {
+		for j := 0; j < len(g.pixels[i]); j++ {
+			fmt.Print(g.pixels[i][j])
+		}
+		fmt.Println()
 	}
 }
--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
-func run() {
+func run () {
 	location1 := Location{1, 1, true}
-	location2 := Location{2, 2, true}
-	location3 := Location{3, 3, true}
-	locations := []Location{location1, location2, location3}
+	location2 := Location{1, 2, true}
+	location3 := Location{1, 3, true}
+	location4 := Location{2, 1, true}
+	location5 := Location{2, 2, true}
+	location6 := Location{2, 3, true}
+	location7 := Location{3, 1, true}
+	location8 := Location{3, 2, true}
+	location9 := Location{3, 3, true}
+
+	locations := []Location{location1, location2, location3,location4, location5, location6,location7, location8, location9}
 	grid := GridInit(10, 10)
 	grid.Load(locations)
 	cfg := pixelgl.WindowConfig{
@@ -22,23 +27,22 @@ func run() {
 		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
+	imd := imdraw.New(nil)
+	imd.EndShape = imdraw.SharpEndShape
+	imd.Color = colornames.Black
+
 	if err != nil {
 		panic(err)
 	}
-
-	imd := imdraw.New(nil)
-	imd.EndShape = imdraw.SharpEndShape
-	for i := 0.0; i < float64(len(grid.pixels)); i++ {
-		imd.Color = colornames.Black
-		fmt.Println(grid.pixels)
-		for j := 0.0; j < float64(len(grid.pixels[int(i)])); j++ {
-			if grid.pixels[int(i)][int(j)] {
-				imd.Push(pixel.V(i, float64(len(grid.pixels[int(i)]))))
-				imd.Line(50)
+	for i := 0; i < len(grid.pixels); i++ {
+		for j := 0; j < len(grid.pixels[i]); j++ {
+			if grid.pixels[i][j] {
+				imd.Push(pixel.V(float64(i*50), float64(j*60)))
+				imd.Circle(10, 25)
 			}
 		}
-
 	}
+	grid.Evaluate()
 
 	for !win.Closed() {
 		win.Clear(colornames.Aliceblue)
@@ -46,8 +50,6 @@ func run() {
 		win.Update()
 	}
 }
-
 func main() {
-
 	pixelgl.Run(run)
 }
